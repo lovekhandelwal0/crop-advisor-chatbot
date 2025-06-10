2128cdec7d60c8423235ff03fe4f20b6bf6e79d0
lovekhandelwal0 <lovekhandelwal0322@gmail.com>
2025-06-10 22:59:38 +1000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import openai
 from openai import OpenAI
 import gdown
 
+from dotenv import load_dotenv
 import os
+
+load_dotenv()  # 🔥 This loads variables from .env into the environment
+
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 sheet_id  = "1JDb4J_pXzhfpryXizHe8v22PiRg6WFSA"
